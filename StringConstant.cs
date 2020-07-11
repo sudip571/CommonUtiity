4df217a2c017f27ea6f404c0a838d99e88a7678a
sudip ranabhat <sudip571@users.noreply.github.com>
2020-07-11 11:24:01 +0545

    Update StringConstant.cs

--- StringConstant.cs
+++ StringConstant.cs
@@ -7,11 +7,7 @@ namespace Logic.CS.BusinessLogic.Adthena
 {
     public sealed class StringConstant
     {
-        // Adthena API URLS
-        public const string INFRINGEMENT_API = @"https://api.adthena.com/wizard/{0}/infringement/all";
-        public const string SEARCH_TERM_DETAIL = @"https://api.adthena.com/wizard/{0}/search-term-detail/all";
-        public const string SEARCH_TERM_OPPORTUNITIES = @"https://api.adthena.com/wizard/{0}/search-term-opportunities/all";
-
+        
         // Session Varaiable Name
         public const string INFRINGEMENTGRID_SESSION = "InfringementGrid";
         public const string MISSINGORGANICGRID_SESSION = "MissingOrganicGrid";
