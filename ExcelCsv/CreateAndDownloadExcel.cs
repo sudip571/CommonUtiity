ace0ebebe8c04a819ea4addd7c4d42cbfadf2601
sudip ranabhat <sudip571@users.noreply.github.com>
2021-12-31 12:40:26 +0545

    Create CreateAndDownloadExcel.cs

---
+++ ExcelCsv/CreateAndDownloadExcel.cs
@@ -0,0 +1,67 @@
+ public byte[] PrepareContentForSearchTermDetailExport(List<SearchTermDetailModified> model)
+        {
+            try
+            {
+                int counter = 2;
+                using (var excelWorkBook = new XLWorkbook())
+                {
+                    var workSheet = excelWorkBook.Worksheets.Add("Search Term Detail");
+                    workSheet.Cell("A1").SetValue("Search Term");
+                    workSheet.Cell("B1").SetValue("Paid Competitors");                    
+                    workSheet.Cell("C1").SetValue("Estimated Organic Clicks");                    
+                    workSheet.Cell("D1").SetValue("Average Organic Position");
+                    workSheet.Cell("E1").SetValue("Top Paid Competitor");
+                    workSheet.Cell("F1").SetValue("Min CPC ($)");
+                    workSheet.Cell("G1").SetValue("Max CPC ($)");
+                    workSheet.Cell("H1").SetValue("Organic CTR (%)");
+
+                    //workSheet.Range("A1:F1").Style.Font.SetBold();
+
+                    foreach (var item in model)
+                    {
+                        workSheet.Cell("A" + counter).SetValue(item.SearchTerm);
+                        workSheet.Cell("B" + counter).SetValue(item.Competitors == -1 ? "-": item.Competitors.ToString());                     
+                        workSheet.Cell("C" + counter).SetValue(item.EstimatedClicks == -1 ? "-" : item.EstimatedClicks.ToString());
+                        workSheet.Cell("D" + counter).SetValue(item.AveragePosition == -1 ? "-" :item.AveragePosition.ToString());
+                        workSheet.Cell("E" + counter).SetValue(item.TopCompetitor == "-1" ? "-": item.TopCompetitor);
+                        workSheet.Cell("F" + counter).SetValue(item.MinCPC == -1 ? "-": item.MinCPC.ToString());
+                        //workSheet.Cell("G" + counter).SetValue(item.MaxCPC == -1 ? "-":item.MaxCPC.ToString("N3"));
+                        workSheet.Cell("G" + counter).SetValue(item.MaxCPC == -1 ? "-" : item.MaxCPC.ToString());
+                        workSheet.Cell("H" + counter).SetValue(item.CTR == -1 ? "-" :(item.CTR * 100).ToString());
+                        counter++;
+                    }
+
+                    //save Excel WorkBook to Memorystream
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        excelWorkBook.SaveAs(memoryStream);
+
+                        //Convert MemoryStream to Byte array.
+                        var bytes = memoryStream.ToArray();
+                        return bytes;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        
+         //if (formatType == "csv")
+ //           {
+ //               extension = ".csv";
+ //               contentType = "text/csv";
+ //           }
+ //           if (formatType == "excel")
+ //           {
+ //               extension = ".xlsx";
+ //               contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+ //           }
+ 
+  //var data = _fileContentPreparation.PrepareContentForSearchTermDetailExport(gridModel);
+  //              var fileName = clientName + " - Search Term Detail" + DateTime.Now.Date.ToString("dd'-'MM'-'yyyy") + extension;
+  //              return File(data, contentType, fileName);
+        
